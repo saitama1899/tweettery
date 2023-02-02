04e5e08feb797a7a5629477200db28a8db52d51d
Eric <eric.lorien92@gmail.com>
2023-02-02 14:31:51 +0100

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Header from "../components/Header"
+
+function Home() {
+  return (
+    <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full ">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
