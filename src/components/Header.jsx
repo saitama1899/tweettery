a6ce0bf9dba6560a4c016517d01e3a36ed7aa79a
Eric <eric.lorien92@gmail.com>
2023-02-02 20:06:38 +0100

    pass props to parent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import Logo from './Logo';
-import Navbar from './Navbar';
-import Searcher from './Searcher';
+import React from 'react'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import Searcher from './Searcher'
 
-function Header() {
+function Header(props) {
+
+  const handleDataFromChild = (data) => {
+    props.onData(data)
+  }
+	const handleErrorFromChild = (error) => {
+		props.onError(error)
+	}
+	const handleLoadingFromChild = (isLoading) => {
+		props.onLoading(isLoading)
+	}
+
 	return (
 		<header>
       <Navbar />
-			<div className='my-8 m-auto shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[4.5px] bg-white/[.33] dark:bg-black/[.6] rounded-xl w-fit py-2 px-16 hover:bg-white/[.5] dark:hover:bg-black/[.7]  duration-500 select-none'>
+			<div className='my-8 m-auto shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[4.5px] bg-white/[.33] dark:bg-black/[.6] rounded-xl py-2 px-16 hover:bg-white/[.5] dark:hover:bg-black/[.7]  duration-500 select-none'>
 				<Logo />
-				<Searcher />
+				<Searcher onData={handleDataFromChild} onError={handleErrorFromChild} onLoading={handleLoadingFromChild} />
 			</div>
 		</header>
 	)
