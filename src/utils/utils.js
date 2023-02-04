e469ea3be6f884b9ea316ce6747c7af3fc7758b0
Eric <eric.lorien92@gmail.com>
2023-02-04 12:52:59 +0100

    add utils for request format

---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export default function separateAndClean(str) {
+  let parts = str.split("  ")
+  let firstPart = parts[1].substr(parts[1].indexOf(":") + 1)
+  let secondPart = parts[2].substr(parts[2].indexOf(":") + 1)
+
+  let firstArray = firstPart.match(/'[^']+'/g).map(x => x.substr(1, x.length - 2))
+  return [firstArray, secondPart.trim()]
+}
