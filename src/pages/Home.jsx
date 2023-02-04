a9075e4ae112b43f98a47259b43bac43c266ca2a
Eric <eric.lorien92@gmail.com>
2023-02-04 12:52:43 +0100

    add summary on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,40 @@
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Header from "../components/Header"
+import Summary from "../components/Summary"
 
 function Home() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-  const handleDataFromParent  = (data) => {
+  useEffect(() => {
+    isLoading && setData(false)
+    error && setData(false)
+  }, [isLoading, error])
+  
+  const handleDataFromParent = (data) => {
     console.log('data', data)
     setData(data)
   }
   const handleErrorFromParent = (error) => {
-    console.log('error', error)
+    setError(error)
+    setTimeout(() => {
+      setError(false)
+    }, 3000)
   }
   const handleLoadingFromParent = (isLoading) => {
-    console.log('isLoading', isLoading)
+    // console.log('isLoading', isLoading)
+    setIsLoading(isLoading)
   }
 
   return (
-    <main className="absolute top-0 right-0 bottom-0 left-0 h-full w-[655px] m-auto">
-      <Header onData={handleDataFromParent } onError={handleErrorFromParent} onLoading={handleLoadingFromParent} />
-      <div className="m-auto w-fit">
-        {/* { data && <Summary />} */}
+    <main className="absolute top-0 right-0 bottom-0 left-0 h-full w-[655px] m-auto max-lg:w-[90%]">
+      <Header onData={handleDataFromParent} onError={handleErrorFromParent} onLoading={handleLoadingFromParent} />
+      <div className="m-auto w-full overflow-x-hidden">
+      { data || isLoading || error ? (
+        <Summary data={data} isLoading={isLoading} error={error} />
+        ) : null }
       </div>
     </main>
   )
