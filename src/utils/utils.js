36e716c4e9eb23090703542354f3ad1268ad9e1e
Eric <eric.lorien92@gmail.com>
2023-02-06 14:51:41 +0100

    improve cohere results

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,15 @@
 export default function separateAndClean(str) {
-  let parts = str.split("  ")
-  let firstPart = parts[1].substr(parts[1].indexOf(":") + 1)
-  let secondPart = parts[2].substr(parts[2].indexOf(":") + 1)
+  let parts = str.split("  ");
+  let firstPart = parts[1].substr(parts[1].indexOf(":") + 1);
+  let secondPart = parts[2].substr(parts[2].indexOf(":") + 1);
 
-  let firstArray = firstPart.match(/'[^']+'/g).map(x => x.substr(1, x.length - 2))
-  return [firstArray, secondPart.trim()]
+  let firstArray = firstPart.match(/'[^']+'/g);
+  if (firstArray) {
+    firstArray = [...new Set(firstArray.map(x => x.substr(1, x.length - 2)))];
+    firstArray = firstArray.map(str => str.split(" ").slice(0, 5).join(" "));
+    firstArray = firstArray.slice(0, 10);
+  } else {
+    firstArray = [];
+  }
+  return [firstArray, secondPart.trim()];
 }
