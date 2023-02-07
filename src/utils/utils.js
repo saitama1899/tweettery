c61e1bb0838405a43b607e24368ba684629f71aa
Eric <eric.lorien92@gmail.com>
2023-02-07 13:56:16 +0100

    hotfix utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,19 @@
 export default function separateAndClean(str) {
   let parts = str.split("  ");
-  let firstPart = parts[1].substr(parts[1].indexOf(":") + 1);
-  let secondPart = parts[2].substr(parts[2].indexOf(":") + 1);
-
-  let firstArray = firstPart.match(/'[^']+'/g);
-  if (firstArray) {
-    firstArray = [...new Set(firstArray.map(x => x.substr(1, x.length - 2)))];
-    firstArray = firstArray.map(str => str.split(" ").slice(0, 5).join(" "));
-    firstArray = firstArray.slice(0, 10);
+  
+  if (parts.length > 0 && parts[1].substr(parts[1].indexOf(":") + 1) && parts[2].substr(parts[2].indexOf(":") + 1)) {
+    let firstPart = parts[1].substr(parts[1].indexOf(":") + 1)
+    let secondPart = parts[2].substr(parts[2].indexOf(":") + 1)
+    let firstArray = firstPart.match(/'[^']+'/g)
+    if (firstArray) {
+      firstArray = [...new Set(firstArray.map(x => x.substr(1, x.length - 2)))]
+      firstArray = firstArray.map(str => str.split(" ").slice(0, 5).join(" "))
+      firstArray = firstArray.slice(0, 10);
+    } else {
+      firstArray = []
+    }
+    return [firstArray, secondPart.trim()]    
   } else {
-    firstArray = [];
+    return [[], "Something went wrong. Try again!"]
   }
-  return [firstArray, secondPart.trim()];
 }
